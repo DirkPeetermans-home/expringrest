a2c423c0f5a4d95135657affd9e0afb8bc88a036
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-10-15 15:04:38 +0200

    ExB1

--- ExB1/src/main/java/be/abis/exercise/service/ApiPersonService.java
+++ ExB1/src/main/java/be/abis/exercise/service/ApiPersonService.java
@@ -1,10 +1,12 @@
 package be.abis.exercise.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import be.abis.exercise.model.Person;
 
+@Service
 public class ApiPersonService implements PersonService {
 	
 	@Autowired
