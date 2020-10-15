3be7231a18cd85fc8059e3b5947f68813817f5ba
Dirk-Peetermans-home <peetermans.dirk@telenet.be>
2020-10-15 14:51:32 +0200

    ExB1

--- ExB1/src/main/java/be/abis/exercise/ExB1Application.java
+++ ExB1/src/main/java/be/abis/exercise/ExB1Application.java
@@ -1,9 +1,15 @@
 package be.abis.exercise;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -16,5 +22,20 @@ public class ExB1Application {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 			return builder.build();
+	}
+	
+	@Bean
+	@Profile("production")
+	public DataSource testDataSource(){
+		return new EmbeddedDatabaseBuilder()
+					.setType(EmbeddedDatabaseType.DERBY)
+					.addScript("classpath:create_course_table_derby.sql")
+					.build();
+	}
+	
+	@Bean
+	@Profile("production")
+	public JdbcTemplate jdbcTemplateDerby(){
+		return new JdbcTemplate(testDataSource());
 	}
 }
